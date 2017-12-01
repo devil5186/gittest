01221c05e2d2536e796c5e24df1be5ae9eb36263
Xin Lin <xlin@stu.xidian.edu.cn>
2017-12-01 17:45:17 +0800

    addup func

--- hello.c
+++ hello.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+int addup(int ,int );
 int main(){
+	printf("%d",addup(0,99));
 	printf("hello world");
 	return 0;
+}
+
+int addup(int begin,int end){
+	int sum = 0;
+	for(int i=begin,i<=end,i++)
+		sum+=i;
+	return sum;
 }
