6c0aa2e9e3476eac94be99b3fbd6417286dee77f
xlin <xlin@stu.xidian.edu.cn>
2017-12-11 18:11:14 +0800

    func before

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-int addup(int ,int );
+int addup(int begin,int end){
+	int sum = 0;
+	for(int i=begin;i<=end;i++)
+		sum+=i;
+	return sum;
+}
 int main(){
 	int sum=0; 
 	for(int i=0;i<100;i++){
@@ -10,9 +15,4 @@ int main(){
 	printf("add up 0~99(func):\n\t%d\n",addup(0,99));
 	return 0;
 }
-int addup(int begin,int end){
-	int sum = 0;
-	for(int i=begin;i<=end;i++)
-		sum+=i;
-	return sum;
-}
+
